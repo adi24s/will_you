a90a2a6f01a9a4584499e8f08cc93ff3c84a0b58
Aditya <ars124c@gmail.com>
2023-02-05 02:38:48 +0530

    add styling to buttons and done with 2nd page

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const maxHeight = window.innerHeight - buttonHeight;
 window.addEventListener('DOMContentLoaded', () => {
     const button = document.getElementById('No');
     const ans_no = document.getElementById('answer--no')
+    const heroBtns = document.querySelectorAll('.hero-btn');
 
     button.addEventListener('click', () => {
         ans_no.style.visibility = 'visible'
@@ -16,5 +17,19 @@ window.addEventListener('DOMContentLoaded', () => {
     button.addEventListener('mouseover', () => {
         button.style.left = Math.floor(Math.random() * (maxWidth + 1)) + 'px';
         button.style.top = Math.floor(Math.random() * (maxHeight + 1)) + 'px';
+    });
+
+
+    heroBtns.forEach(function (btn) {
+        btn.addEventListener('click', function () {
+            const parent = btn.parentElement;
+            if (!parent.classList.contains('active')) {
+                parent.classList.add('active');
+                setTimeout(function () {
+                    parent.classList.remove('active');
+                    window.location.href = "index3.html";
+                }, 2000);
+            }
+        });
     });
 });
